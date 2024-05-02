9b5abba564f251606b14cebd66597c39d8fb66b5
Henry Ye Cao <henryyecao@henrys-mbp.home>
2024-05-02 01:55:11 -0400

    blog writer update

--- backend/auth.py
+++ backend/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask import Blueprint, render_template, redirect, url_for, request
 from werkzeug.security import generate_password_hash
 from .models import Blog_credentials
 from . import db
