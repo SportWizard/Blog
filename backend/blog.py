aefda81593904f08c16a53a5c7d3a92851632d22
Henry Ye Cao <45925659+SportWizard@users.noreply.github.com>
2025-04-26 13:08:53 -0400

    blog writer

--- backend/blog.py
+++ backend/blog.py
@@ -1,9 +1,8 @@
 from flask import Blueprint, render_template
-from .models import Blog_credentials
-from . import db
+from .models import Blog_credentials, Post
 
 blog = Blueprint("blog", __name__)
 
 @blog.route("/blog")
-def blog_page():
-    return render_template("blog.html", blog_credentials=Blog_credentials.query.first())
+def blog_page(): #the function cannot be the same name as the blueprint
+    return render_template("blog.html", blog_credentials=Blog_credentials.query.first(), posts=Post.query.all())
